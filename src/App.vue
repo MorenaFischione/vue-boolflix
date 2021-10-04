07ce6c5f8e116273095322e2b2dc934017f38f2c
MorenaFischione <fischione.morena@gmail.com>
2021-10-04 16:09:10 +0200

    Milestone 3

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,70 @@
 <template>
   <div id="app">
-    <Container />
+    <Header @search="cerca" />
+    <Main :movies="cards" :tvSerie="tvShows"/>
   </div>
 </template>
 
 <script>
-import Container from './components/Container.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import axios from "axios";
+
 
 export default {
   name: 'App',
+
+  data()  {
+    return {
+      keyApi : "8f9f51349841d6d63cd953856f92bc86",
+      apiFilmsLink : "https://api.themoviedb.org/3/search/movie",
+      apiSerieLink : "https://api.themoviedb.org/3/search/tv",
+      query : "neddle",
+      cards : [],
+      tvShows : [],
+    }
+  },
+
   components: {
-    Container,
-  }
+    Header,
+    Main
+  },
+
+  methods: {
+    cerca : function(neddlePassato) {
+      axios.get( this.apiFilmsLink, {
+        params: {
+          api_key : this.keyApi,
+          query: neddlePassato,
+          language: "it-IT",
+        }
+      })
+      .then((response) => {
+      const result = response.data;
+      const risultato = result.results;
+      this.cards = [...risultato];
+      });
+
+      axios.get( this.apiSerieLink, {
+        params: {
+          api_key : this.keyApi,
+          query: neddlePassato,
+          language: "it-IT",
+        }
+      })
+      .then((response) => {
+      const result = response.data;
+      const risultato = result.results;
+      this.tvShows = [...risultato];
+      });
+    }
+  },
 }
 </script>
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap";
-
+  
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
